43c5fa8d248f588494ad8b0f67f2a50a4beabc71
León Domingo <leon.domingo@ender.es>
2010-12-28 19:13:55 +0100

    Cambiar ejemplo.py

--- libpy/default/update/ejemplo/ejemplo.py
+++ libpy/default/update/ejemplo/ejemplo.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import os
+#import os
 import sys
-sys.path.append(os.getcwd())
-from config import CONFIG
+#sys.path.append(os.getcwd())
+from ..config import CONFIG
 sys.path = sys.path + CONFIG['paths']
 
 if __name__ == '__main__':
