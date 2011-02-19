e289a49530ce2f397313b190e3c4d1f569e5ff77
León Domingo <leon.domingo@ender.es>
2011-02-19 14:18:00 +0100

    Mantis #8248:  - Poder adjuntar ficheros al enviar emails  - Función "génerica" para obtener el template basado en la variable "ruta_templates"

--- libpy/enviaremail.py
+++ libpy/enviaremail.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import os
 import smtplib
+from email import Encoders
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
 
 def enviar_email(remitente, destinatarios, asunto, mensaje, 
-                 servidor, login, password):
+                 servidor, login, password, ficheros=[]):
     """
     IN
       remitente (<email>, <nombre>)
@@ -25,6 +29,21 @@ def enviar_email(remitente, destinatarios, asunto, mensaje,
     """
     
     msg = MIMEText(mensaje)
+    
+    if ficheros:
+        msg_f = MIMEMultipart()
+        msg_f.attach(msg)
+        
+        msg = msg_f
+        
+        for f in ficheros:
+            part = MIMEBase('application', 'otect-stream')
+            part.set_payload(open(f, 'rb').read())
+            Encoders.encode_base64(part)
+            part.add_header('Content-Disposition', 
+                            'attachment; filename="%s"' % os.path.basename(f))
+            msg.attach(part)
+            
     msg['Subject'] = asunto
     msg['From'] = '%s <%s>' % (remitente[1], remitente[0])
     
@@ -59,19 +78,3 @@ def enviar_email(remitente, destinatarios, asunto, mensaje,
             
     except Exception, e:
         print str(e)
-    
-if __name__ == '__main__':
-    
-    import cStringIO
-    
-    mensaje = cStringIO.StringIO()    
-    mensaje.write('ESTO ES OTRA pruebita')
-    mensaje.seek(0)
-    
-    r = enviar_email(('leon.domingo@ender.es', 'León Domingo Ortín'),
-                     [('tengounplanb@gmail.com', 'León Domingo Ortín'),
-                      ('leon.domingo@gmail.com', 'Pepe Pérez')],
-                     'Esto es una prueba', mensaje.read(),
-                     'smtp.gmail.com', 'leon.domingo@ender.es', '5390ld')
-    
-    print r
