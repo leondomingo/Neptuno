df990e78a62e12df89c5f304df6bd472db4c2bff
León Domingo <leon.domingo@ender.es>
2011-03-16 15:40:12 +0100

    Minimizar el uso de estilos

--- libpy/xlsreport.py
+++ libpy/xlsreport.py
@@ -4,10 +4,11 @@ import re
 import xlrd
 from lxml import etree
 from xlwt import Workbook, Formula
-from xlwt.Style import easyxf
+from xlwt.Style import easyxf, XFStyle
 from genshi.template.loader import TemplateLoader
 from libpy.util import strtodate, strtotime
 from nucleo.config import VARIABLES
+from libpy.log import NeptunoLogger
 
 def xlsreport(template, params, filename):
     """
@@ -19,6 +20,8 @@ def xlsreport(template, params, filename):
     OUT
       <str>
     """
+    
+    logger = NeptunoLogger.get_logger('xlsreport')
 
     class Util(object):
         
@@ -43,14 +46,213 @@ def xlsreport(template, params, filename):
         def bookmark(self, name):
             return self.marks[name]
             
+    def calcular_estilo(estilo):
+        
+        style = []
+        
+        if estilo != None:
+            # font
+            fuente = estilo.find('font')
+            if fuente != None:
+                
+                font = []
+                
+                # name
+                if fuente.attrib.get('name', None):
+                    font.append('name %s' % fuente.attrib['name'])
+                    
+                # size (height)
+                if fuente.attrib.get('size', None):
+                    font.append('height %d' % (int(fuente.attrib['size'])*25))
+                    
+                # color
+                if fuente.attrib.get('color', None):
+                    color = fuente.attrib['color']
+                    font.append('color %s' % color)
+                    
+                # bold
+                if fuente.attrib.get('bold', None):
+                    font.append('bold %s' % fuente.attrib['bold'])
+                
+                # italic
+                if fuente.attrib.get('italic', None):
+                    font.append('italic %s' % fuente.attrib['italic'])
+                    
+                # underline
+                if fuente.attrib.get('underline', None):
+                    font.append('underline %s' % fuente.attrib['underline'])
+                
+                # añadir "font"
+                if font:
+                    #print font
+                    style.append('font: %s' % (','.join(font)))
+                    
+            # align
+            if estilo.find('align') != None:
+                
+                al = estilo.find('align')
+                align = []
+                
+                # halign (horiz align)
+                if al.attrib.get('hor', None):
+                    align.append('horiz %s' % al.attrib['hor'])
+                    
+                # valign (vert align)
+                if al.attrib.get('ver', None):
+                    align.append('vert %s' % al.attrib['ver'])
+                    
+                if align:
+                    style.append('align: %s' % (','.join(align)))
+                    
+            pattern = []
+            
+            # bgcolor
+            patron_relleno = ''
+            if estilo.attrib.get('pattern', None):
+                patron_relleno = estilo.attrib['pattern']
+                pattern.append('pattern %s' % patron_relleno)
+            
+            if estilo.attrib.get('bgcolor', None):
+                if not patron_relleno:
+                    pattern.append('pattern solid')
+                    
+                pattern.append('fore-color %s' % estilo.attrib['bgcolor'])
+                
+            if pattern:
+                style.append('pattern: %s' % (','.join(pattern)))
+                
+            border = []
+            if estilo.find('border') != None:
+                borde = estilo.find('border')
+                
+                # color
+                color_borde = ''
+                if borde.attrib.get('color', None):
+                    color_borde = borde.attrib['color']
+                
+                # line
+                linea_borde = 'thin'
+                if borde.attrib.get('line', None):
+                    linea_borde = borde.attrib['line']
+                
+                # top
+                top_color = ''
+                top_line = ''
+                if borde.find('top') != None:
+                    tp = borde.find('top')
+                    if tp.attrib.get('color', None):
+                        top_color = tp.attrib['color']
+                        
+                    elif color_borde:
+                        top_color = color_borde
+                        
+                    if tp.attrib.get('line', None):
+                        top_line = tp.attrib['line']
+                    
+                    elif linea_borde:
+                        top_line = linea_borde
+                        
+                if top_color:
+                    border.append('top_color %s' % top_color)
+                    
+                if top_line:
+                    border.append('top %s' % top_line)
+                
+                # bottom
+                bottom_color = ''
+                bottom_line = ''
+                if borde.find('bottom') != None:
+                    bt = borde.find('bottom')
+                    if bt.attrib.get('color', None):
+                        bottom_color = bt.attrib['color']
+                        
+                    elif color_borde:
+                        bottom_color = color_borde
+                        
+                    if bt.attrib.get('line', None):
+                        bottom_line = bt.attrib['line']
+                    
+                    elif linea_borde:
+                        bottom_line = linea_borde
+
+                if bottom_color:
+                    border.append('bottom_color %s' % bottom_color)
+                    
+                if bottom_line:
+                    border.append('bottom %s' % bottom_line)
+                
+                # left
+                left_color = ''
+                left_line = ''
+                if borde.find('left') != None:
+                    lf = borde.find('left')
+                    if lf.attrib.get('color', None):
+                        left_color = lf.attrib['color']
+                        
+                    elif color_borde:
+                        left_color = color_borde
+                        
+                    if lf.attrib.get('line', None):
+                        left_line = lf.attrib['line']
+                    
+                    elif linea_borde:
+                        left_line = linea_borde
+
+                if left_color:
+                    border.append('left_color %s' % left_color)
+                    
+                if left_line:
+                    border.append('left %s' % left_line)
+
+                # right
+                right_color = ''
+                right_line = ''
+                if borde.find('right') != None:
+                    rt = borde.find('right')
+                    if rt.attrib.get('color', None):
+                        right_color = rt.attrib['color']
+                        
+                    elif color_borde:
+                        right_color = color_borde
+                        
+                    if rt.attrib.get('line', None):
+                        right_line = rt.attrib['line']
+                    
+                    elif linea_borde:
+                        right_line = linea_borde
+
+                if right_color:
+                    border.append('right_color %s' % right_color)
+                    
+                if right_line:
+                    border.append('right %s' % right_line)
+
+                if border:
+                    style.append('border: %s' % (','.join(border)))
+                    
+        return style
+                    
+    logger.debug(0)
+    
     tl = TemplateLoader([VARIABLES['ruta_templates']])
     tmpl = tl.load(template)
-    informe = tmpl.generate(**params)
+    logger.debug('Template cargado!')
+    
+    informe = tmpl.generate(**params)    
+    logger.debug('Template generado!')
+    
     informe_xml = informe.render('xml')
+    logger.debug('Informe XML generado!')
+    
+    logger.debug(1)
     
     root = etree.fromstring(informe_xml)
     
     wb = Workbook()
+    
+    estilos = {}
+    
+    logger.debug(2)
     
     n_sheet = 0
     for sheet in root.iter('sheet'):
@@ -66,11 +268,19 @@ def xlsreport(template, params, filename):
         
         util = Util()
         for item in sheet:
+            
+            # style
+            if item.tag == 'style':
+                logger.debug('<style>')
+                
+                estilos[item.attrib['name']] = easyxf(';'.join(calcular_estilo(item))) 
+            
             # cell
             if item.tag == 'cell':
+                logger.debug('<cell>')
+                
                 col = int(item.attrib['col'])
                 
-                style = []
                 num_format = None
                 
                 value = item.find('value')
@@ -105,8 +315,11 @@ def xlsreport(template, params, filename):
                     
                     # resto de tipos
                     else:
-                        f = lambda t,v: t(v)
-                        dato = f(eval(tipo[0]), dato)
+                        try:
+                            f = lambda t,v: t(v)
+                            dato = f(eval(tipo[0]), dato)
+                        except:
+                            dato = 'ERROR!'
                     
                     # date;dd/mm/yyyy
                     # date;dd/mmm/yyyy
@@ -120,194 +333,30 @@ def xlsreport(template, params, filename):
 
                 # style: aplicar un estilo
                 estilo = item.find('style')
+                xfs = None
                 if estilo != None:
-                    # font
-                    fuente = estilo.find('font')
-                    if fuente != None:
+                    if not estilos.has_key(estilo.attrib['name']):
+                        xfs = easyxf(';'.join(calcular_estilo(estilo))) 
+    
+                        estilos[estilo.attrib['name']] = xfs
                         
-                        font = []
+                    else:
+                        xfs = estilos[estilo.attrib['name']]
                         
-                        # name
-                        if fuente.attrib.get('name', None):
-                            font.append('name %s' % fuente.attrib['name'])
-                            
-                        # size (height)
-                        if fuente.attrib.get('size', None):
-                            font.append('height %d' % (int(fuente.attrib['size'])*25))
-                            
-                        # color
-                        if fuente.attrib.get('color', None):
-                            color = fuente.attrib['color']
-                            font.append('color %s' % color)
-                            
-                        # bold
-                        if fuente.attrib.get('bold', None):
-                            font.append('bold %s' % fuente.attrib['bold'])
+                if num_format:
+                    if not xfs:
+                        xfs = XFStyle()
                         
-                        # italic
-                        if fuente.attrib.get('italic', None):
-                            font.append('italic %s' % fuente.attrib['italic'])
-                            
-                        # underline
-                        if fuente.attrib.get('underline', None):
-                            font.append('underline %s' % fuente.attrib['underline'])
-                        
-                        # añadir "font"
-                        if font:
-                            #print font
-                            style.append('font: %s' % (','.join(font)))
-                            
-                    # align
-                    if estilo.find('align') != None:
-                        
-                        al = estilo.find('align')
-                        align = []
-                        
-                        # halign (horiz align)
-                        if al.attrib.get('hor', None):
-                            align.append('horiz %s' % al.attrib['hor'])
-                            
-                        # valign (vert align)
-                        if al.attrib.get('ver', None):
-                            align.append('vert %s' % al.attrib['ver'])
-                            
-                        if align:
-                            style.append('align: %s' % (','.join(align)))
-                            
-                    pattern = []
-                    
-                    # bgcolor
-                    patron_relleno = ''
-                    if estilo.attrib.get('pattern', None):
-                        patron_relleno = estilo.attrib['pattern']
-                        pattern.append('pattern %s' % patron_relleno)
-                    
-                    if estilo.attrib.get('bgcolor', None):
-                        if not patron_relleno:
-                            pattern.append('pattern solid')
-                            
-                        pattern.append('fore-color %s' % estilo.attrib['bgcolor'])
-                        
-                    if pattern:
-                        style.append('pattern: %s' % (','.join(pattern)))
-                        
-                    border = []
-                    if estilo.find('border') != None:
-                        borde = estilo.find('border')
-                        
-                        # color
-                        color_borde = ''
-                        if borde.attrib.get('color', None):
-                            color_borde = borde.attrib['color']
-                        
-                        # line
-                        linea_borde = 'thin'
-                        if borde.attrib.get('line', None):
-                            linea_borde = borde.attrib['line']
-                        
-                        # top
-                        top_color = ''
-                        top_line = ''
-                        if borde.find('top') != None:
-                            tp = borde.find('top')
-                            if tp.attrib.get('color', None):
-                                top_color = tp.attrib['color']
-                                
-                            elif color_borde:
-                                top_color = color_borde
-                                
-                            if tp.attrib.get('line', None):
-                                top_line = tp.attrib['line']
-                            
-                            elif linea_borde:
-                                top_line = linea_borde
-                                
-                        if top_color:
-                            border.append('top_color %s' % top_color)
-                            
-                        if top_line:
-                            border.append('top %s' % top_line)
-                        
-                        # bottom
-                        bottom_color = ''
-                        bottom_line = ''
-                        if borde.find('bottom') != None:
-                            bt = borde.find('bottom')
-                            if bt.attrib.get('color', None):
-                                bottom_color = bt.attrib['color']
-                                
-                            elif color_borde:
-                                bottom_color = color_borde
-                                
-                            if bt.attrib.get('line', None):
-                                bottom_line = bt.attrib['line']
-                            
-                            elif linea_borde:
-                                bottom_line = linea_borde
+                    xfs.num_format_str = num_format
 
-                        if bottom_color:
-                            border.append('bottom_color %s' % bottom_color)
-                            
-                        if bottom_line:
-                            border.append('bottom %s' % bottom_line)
-                        
-                        # left
-                        left_color = ''
-                        left_line = ''
-                        if borde.find('left') != None:
-                            lf = borde.find('left')
-                            if lf.attrib.get('color', None):
-                                left_color = lf.attrib['color']
-                                
-                            elif color_borde:
-                                left_color = color_borde
-                                
-                            if lf.attrib.get('line', None):
-                                left_line = lf.attrib['line']
-                            
-                            elif linea_borde:
-                                left_line = linea_borde
-
-                        if left_color:
-                            border.append('left_color %s' % left_color)
-                            
-                        if left_line:
-                            border.append('left %s' % left_line)
-
-                        # right
-                        right_color = ''
-                        right_line = ''
-                        if borde.find('right') != None:
-                            rt = borde.find('right')
-                            if rt.attrib.get('color', None):
-                                right_color = rt.attrib['color']
-                                
-                            elif color_borde:
-                                right_color = color_borde
-                                
-                            if rt.attrib.get('line', None):
-                                right_line = rt.attrib['line']
-                            
-                            elif linea_borde:
-                                right_line = linea_borde
-
-                        if right_color:
-                            border.append('right_color %s' % right_color)
-                            
-                        if right_line:
-                            border.append('right %s' % right_line)
-
-                        if border:
-                            style.append('border: %s' % (','.join(border)))
-                                            
-#                print style
-                ws.write(util.current_line(), col, dato, 
-                         easyxf(';'.join(style), num_format_str=num_format))
+                ws.write(util.current_line(), col, dato, xfs)
             
             elif item.tag == 'line_feed':
+                logger.debug('<line_feed>')
                 util.line_feed()
                 
             elif item.tag == 'bookmark':
+                logger.debug('<bookmark>')
                 util.add_bookmark(item.attrib['name'])
             
     wb.save(filename)
