120186fa3f16dcc07a9093f1facc74c857099190
León Domingo <leon.domingo@ender.es>
2011-04-02 10:30:08 +0200

    Corregir "description"

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup
 
 setup(name='Neptuno2',
-      version='1.0',
+      version='1.01',
       author='León Domingo',
       author_email='leon.domingo@ender.es',
-      descripcion=('A little set of utilities that Ender has been using in their web projects'),
+      descripcion='A little set of utilities that Ender has been using in their web projects',
       #license=???,
       keywords='neptuno ender web',
       classifiers=[
