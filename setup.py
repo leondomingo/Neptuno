c8f30e10c587e705470d5125a006873f6301b1f7
León Domingo <leon.domingo@ender.es>
2011-05-25 20:46:47 +0200

    Corregir configuración de instalación de Neptuno2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='Neptuno2',
-      version='1.0.18',
+      version='1.0.19',
       author='León Domingo',
       author_email='leon.domingo@ender.es',
       description=('A little set of utilities that Ender has been using in their web projects'),
@@ -15,7 +15,14 @@ setup(name='Neptuno2',
       ],
       url='www.ender.es',      
       packages=['neptuno',
-                'neptuno.postgres'],
+                'neptuno.postgres',
+                'neptuno.templates',
+      ],
+      package_data={
+            'neptuno.default': ['*.*'],
+            'neptuno.default.update': ['*.*'],
+            'neptuno.templates': ['*.xml'],
+      },
       install_requires=[
         'psycopg2',
         'SQLAlchemy==0.6.7',
