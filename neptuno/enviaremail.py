b6ec4754e32c647fe93dc13f5d2c486d5cba4fb3
León Domingo <leon.domingo@ender.es>
2012-07-29 15:00:16 +0200

    Change "enviaremail.enviar_email" (spanish) for "sendmail.send_mail" / Some corrections with attachments

--- neptuno/enviaremail.py
+++ neptuno/enviaremail.py
@@ -1,124 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import os
-import smtplib
-from email import Encoders
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email.header import Header
+from sendmail import send_mail
 
 def enviar_email(remitente, destinatarios, asunto, mensaje, 
                  servidor, login, password, ficheros=[], html='', cc=[], bcc=[], 
                  charset='iso-8859-1'):
-    """
-    IN
-      remitente      (<email>, <nombre>)
-      destinatarios  [(<email>, <nombre>), ...]
-      asunto         <str>
-      mensaje        <str>
-      servidor       <str>
-      login          <str>
-      password       <str>
-      ficheros       [<str>, ...] (opcional)
-                     [(<StringIO>, <str>), ...] (opcional)
-      html           <str>        (opcional)
-      cc             [(<e-mail>, <nombre>), ...]
-      bcc            [(<e-mail>, <nombre>), ...]
-      charset        <str> => iso-8859-1
-      
-      Por ejemplo,
-        enviar_email(('pperez@gmail.com', 'Pepe Pérez'),
-                     [('malopez@gmail.com', 'María López'), (mlopez@yahoo.com, 'María')],
-                     'Hola, mundo!', '¿Qué tal por ahí?\n\nPepe',
-                     'smtp.gmail.com', 
-                     'pperez@gmail.com', 'ilovemarialopez')
-                     
-    """
     
-    msg = MIMEText(mensaje)
-    msg.set_type('text/plain')
-    msg.set_charset(charset)
+    send_mail(remitente, destinatarios, asunto, mensaje, servidor, login, password,
+              files=ficheros, html=html, cc=cc, bcc=bcc, charset=charset)
     
-    msg_root = None
-    if html:
-        msg_root = MIMEMultipart('related')
-        msg_root.preamble = 'This is a multi-part message in MIME format.'
-        
-        if html:
-            msg_alt = MIMEMultipart('alternative')
-            
-            msg_alt.attach(msg)
-            
-            msg_html = MIMEText(html)
-            msg_html.set_type('text/html')
-            msg_html.set_charset(charset)
-
-            msg_alt.attach(msg_html)
-            
-            msg_root.attach(msg_alt)
-            
-        msg = msg_root
-        
-    if ficheros:
-        if not msg_root:
-            msg_root = MIMEMultipart()
-            msg_root.attach(msg)
-            msg = msg_root
-            
-        for f in ficheros:
-            part = MIMEBase('application', 'octet-stream')
-            if isinstance(f, str):
-                f = open(f, 'rb')
-                f_name = os.path.basename(f)
-                
-            else:
-                # tupla (<fichero>, <nombre>,)
-                f, f_name = f
-                
-            part.set_payload(f.read())
-            Encoders.encode_base64(part)
-            part.add_header('Content-Disposition', 
-                            'attachment; filename="%s"' % f_name)
-            msg.attach(part)
-            
-    msg['Subject'] = Header(asunto, charset)
-    msg['From'] = '%s <%s>' % (remitente[1], remitente[0])
-    
-    nombres = ['%s <%s>' % (dst[1], dst[0]) for dst in destinatarios]
-    msg['To'] = ';'.join(nombres)
-    
-    msg['Cc'] = ';'.join(['%s <%s>' % (dst[1], dst[0]) for dst in cc])
-        
-    s = smtplib.SMTP(servidor)
-    try:
-        s.set_debuglevel(False)
-        
-        # autenticarse con el servidor
-        s.ehlo()
-        
-        # tiene STARTTLS?
-        if s.has_extn('STARTTLS'):
-            s.starttls()
-            s.ehlo() # re-identify ourselves over TLS connection
-            
-        # login
-        s.login(login, password)
-        
-        emails_destino_to = [dst[0] for dst in destinatarios]
-        emails_destino_cc = [dst[0] for dst in cc]
-        emails_destino_bcc = [dst[0] for dst in bcc]
-        
-        emails_destino = emails_destino_to + emails_destino_cc + emails_destino_bcc 
-        
-        # enviar e-mail
-        resultado = s.sendmail(remitente[0], emails_destino, msg.as_string())
-        
-        return resultado
-    
-    finally:
-        s.quit()
-        
 def normalizar_a_html(texto):
     return texto.\
         replace('\'', '&#39;').\
---
+++ neptuno/sendmail.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+
+import os
+import smtplib
+from email import Encoders
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.header import Header
+
+def send_mail(from_, to_, subject, message, server, login, password, files=[], 
+              html='', cc=[], bcc=[], charset='utf-8'):
+    """
+    IN
+      from_          (<e-mail address>, <name>)
+      to_            [(<e-mail address>, <name>), ...]
+      subject        <str>
+      message        <str>
+      server         <str>
+      login          <str>
+      password       <str>
+      files          [<str>, ...] (optional)
+                     [(<StringIO>, <str>), ...] (optional)
+      html           <str>        (optional)
+      cc             [(<e-mail address>, <name>), ...]
+      bcc            [(<e-mail address>, <name>), ...]
+      charset        <str> => utf-8
+      
+      For example,
+        send_mail(('pperez@gmail.com', 'Pepe Pérez'),
+                  ['malopez@gmail.com', 'María López'), (mlopez@yahoo.com, 'María')],
+                  'Hello, world!', 'How you doing?\n\nPepe',
+                  'smtp.gmail.com', 
+                  'pperez@gmail.com', 'ilovemarialopez')
+                     
+    """
+    
+    msg = MIMEText(message)
+    msg.set_type('text/plain')
+    msg.set_charset(charset)
+    
+    msg_root = None
+    if html:
+        msg_root = MIMEMultipart('related')
+        msg_root.preamble = 'This is a multi-part message in MIME format.'
+        
+        if html:
+            msg_alt = MIMEMultipart('alternative')
+            
+            msg_alt.attach(msg)
+            
+            msg_html = MIMEText(html)
+            msg_html.set_type('text/html')
+            msg_html.set_charset(charset)
+
+            msg_alt.attach(msg_html)
+            
+            msg_root.attach(msg_alt)
+            
+        msg = msg_root
+        
+    if files:
+        if not msg_root:
+            msg_root = MIMEMultipart()
+            msg_root.attach(msg)
+            msg = msg_root
+            
+        files_obj = []
+        for f in files:
+            part = MIMEBase('application', 'octet-stream')
+            if isinstance(f, (str, unicode,)):
+                f_name = os.path.basename(f)
+                
+                f = open(f, 'rb')
+                files_obj.append(f)
+                
+            else:
+                # tuple (<file-like>, <file name>,)
+                f, f_name = f
+                f.seek(0)
+                
+            part.set_payload(f.read())
+            Encoders.encode_base64(part)
+            part.add_header('Content-Disposition', 
+                            'attachment; filename="%s"' % f_name)
+            msg.attach(part)
+            
+    msg['Subject'] = Header(subject, charset)
+    msg['From'] = '%s <%s>' % (from_[1], from_[0])
+    
+    nombres = ['%s <%s>' % (dst[1], dst[0]) for dst in to_]
+    msg['To'] = ';'.join(nombres)
+    
+    msg['Cc'] = ';'.join(['%s <%s>' % (dst[1], dst[0]) for dst in cc])
+        
+    s = smtplib.SMTP(server)
+    try:
+        s.set_debuglevel(False)
+        
+        # authenticate to the server
+        s.ehlo()
+        
+        # STARTTLS?
+        if s.has_extn('STARTTLS'):
+            s.starttls()
+            s.ehlo() # re-identify ourselves over TLS connection
+            
+        # login
+        s.login(login, password)
+        
+        emails_dst_to = [dst[0] for dst in to_]
+        emails_dst_cc = [dst[0] for dst in cc]
+        emails_dst_bcc = [dst[0] for dst in bcc]
+        
+        emails_dst = emails_dst_to + emails_dst_cc + emails_dst_bcc 
+        
+        # send e-mail
+        result = s.sendmail(from_[0], emails_dst, msg.as_string())
+        
+        # close files (if there's any)
+        for f in files_obj:
+            f.close()
+        
+        return result
+    
+    finally:
+        s.quit()
+        
+def normalizar_a_html(texto):
+    return texto.\
+        replace('\'', '&#39;').\
+        replace('&', '&amp;').\
+        replace('#', '&#35;').\
+        replace('á', '&aacute;').\
+        replace('Á', '&Aacute;').\
+        replace('é', '&eacute;').\
+        replace('É', '&Eacute;').\
+        replace('í', '&iacute;').\
+        replace('Í', '&Iacute;').\
+        replace('ó', '&oacute;').\
+        replace('Ó', '&Oacute;').\
+        replace('ú', '&uacute;').\
+        replace('Ú', '&Uacute;').\
+        replace('ü', '&uuml;').\
+        replace('Ü', '&Uuml;').\
+        replace('ñ', '&ntilde;').\
+        replace('Ñ', '&Ntilde;').\
+        replace('<', '&lt;').\
+        replace('>', '&gt;').\
+        replace('¡', '&iexcl;').\
+        replace('?', '&iquest;').\
+        replace('"', '&quot;').\
+        replace('%', '&#37;')
+        
